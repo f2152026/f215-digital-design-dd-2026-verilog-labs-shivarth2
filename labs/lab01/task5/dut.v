1191c219e259f594b95c17b3802dd272cea3ed3b
shivarth2 <f20241025@pilani.bits-pilani.ac.in>
2026-08-29 11:36:57 +0000

    lab01 task5

--- labs/lab01/task5/dut.v
+++ labs/lab01/task5/dut.v
@@ -13,12 +13,12 @@ module dut(
 );
 
   // ---- Bonus: hierarchical (O(log n)) 64-bit carry-lookahead adder ----
-  cla64_hier U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+  //cla64_hier U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 
   // ---- For comparison, copy the required file(s) into this folder and
   //      uncomment ONE of the options below at a time (matching Task 4) ----
   // rca64 U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
   // cla64_flat U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
-  // cla64_blocked U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+  cla64_blocked U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 
 endmodule
